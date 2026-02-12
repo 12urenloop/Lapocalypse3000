9d82ac61bb4973151e213240a1793d1ec5dc686f
robinpdev <44908176+robinpdev@users.noreply.github.com>
2026-02-12 20:10:01 +0100

    make library work on platformIO

--- code/ranging/dwmware_v3/src/main.cpp
+++ code/ranging/dwmware_v3/src/main.cpp
@@ -1,18 +1,10 @@
-#include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#pragma once
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+#ifdef TAGCODE
+#include "tag.h"
+#endif
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+#ifdef ANCHORCODE
+#include "anchor.h"
+#endif
